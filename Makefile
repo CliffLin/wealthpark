c30abd5c71052a6f72445ee26077c87660487bb8
Cliff Lin <cliff.lin@cobinhood.com>
2019-10-15 21:56:49 +0800

    misc: remove redundant file and update gitignore

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	ENV=TEST pytest -s
+
+dev-server:
+	./bin/api
+
+prod-server:
+	ENV=prod ./bin/api
--- wealthpark/config.py
+++ wealthpark/config.py
@@ -4,10 +4,15 @@ import os
 class Config:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////%s/runtime/database.db' % os.environ.get('GITROOT')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////%s' % os.environ.get('GITROOT') + \
+                              '/runtime/database.db'
+    SERVER_NAME = '127.0.0.1:9999'
 
-class Producation(Config):
+class Production(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////%s' % os.environ.get('GITROOT') + \
+                              '/runtime/prod_database.db'
     DEBUG = False
+    SERVER_NAME = '0.0.0.0:8080'
 
 class Development(Config):
     ENV = 'development'
@@ -15,4 +20,5 @@ class Development(Config):
 class Testing(Config):
     ENV = 'development'
     TEST = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////%s/runtime/test_database.db' % os.environ.get('GITROOT')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////%s' % os.environ.get('GITROOT') + \
+                              '/runtime/test_database.db'
